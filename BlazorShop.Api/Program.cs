883e54d4568bfb3710a92580e950b31badaf3098
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-02-03 00:19:00 -0300

    Adicionando Cors e implementando conexao da banco com produtos

--- BlazorShop.Api/Program.cs
+++ BlazorShop.Api/Program.cs
@@ -2,6 +2,7 @@ using BlazorShop.Api.Context;
 using BlazorShop.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Headers;
 using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,13 +17,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();    
-
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
 var app = builder.Build();
 
@@ -32,6 +34,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(policy =>
+    policy.WithOrigins("https://localhost:7258", "https://localhost:7258")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithHeaders(HeaderNames.ContentType)
+);
+
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
